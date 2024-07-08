9a6aa215714aae014000e4ddfb9c3ebff949d0bb
adityadebnath009 <debnathaditya2005@gmail.com>
2024-07-08 15:32:25 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,26 @@
+const accountId = 1233455
+let accountEmail = "example@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+accountCity="Bengaluru"
+accountPassword ="424763"
+accountEmail = "aditya@gmail.com"
+
+
+
+/*
+Prefer not to use var
+beacause of issue in block scope and functional scope
+ */
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";
+// treat all js code as newer version
+
+// alert("Hello") We are using nodejs not browser
+
+
+let name="Aditya"
+let age = 19
+
+
+//number
+// bigint
+// String-----""
+// boolean-----true/false
+// null----standalone value
+// undefined
+// Symbol
+
+
+console.log(null)
+console.log(undefined)
