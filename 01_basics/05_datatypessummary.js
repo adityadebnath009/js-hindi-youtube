3165be1398d6ca3fa9a7b9901974f09b99e22130
adityadebnath009 <debnathaditya2005@gmail.com>
2024-07-10 11:10:15 +0000

    container config done

---
+++ 01_basics/05_datatypessummary.js
@@ -0,0 +1,53 @@
+/*Primitives---->
+7 types : Strings, NUmber ,Boolean ,null ,undefined ,Symbol ,BigInt*/
+
+// JavaScript is a dynamically typed language.
+// This means that:
+// Type checking happens at runtime:
+// The type of a variable is determined when the code is executed, not when it's written.
+// Variables can hold values of different types:
+// You can assign a number to a variable, and later assign a string to the same variable without any issues.
+
+
+const Id =Symbol("123")
+const anotherId = Symbol("123")
+console.log(typeof anotherId)//Type--Symbol
+
+console.log(Id == anotherId)//It will result as false
+
+
+const bigNumber = 34567827738927739n //adding a 'n' to the last makes the variable bigInt
+console.log(typeof bigNumber)
+
+
+
+//Reference (Non-Primitive)
+//Array ,Objects ,Functions
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++===============================================
+
+
+//Stack (Primitive) , Heap(Non-Primitive)
+
+
+
+
+let myYoutubename = "hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+console.log(anothername)
+
+anothername = "chaiaurcode"
+
+console.log(myYoutubename)
+console.log(anothername)
+
+
+
+let userOne = {
+    email:"user@gmail.com",
+    upi  : "6917319731"
+}
+
+
