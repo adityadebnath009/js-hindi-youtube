afe8c03692641c60b2956c427a4c13bb7c6737b6
adityadebnath009 <debnathaditya2005@gmail.com>
2024-07-11 15:16:30 +0000

    container config done

---
+++ 01_basics/09_date_time.js
@@ -0,0 +1,13 @@
+//Dates
+
+let mydate = new Date()
+console.log(mydate.getDate())
+console.log(mydate.getDay())
+console.log(mydate.getFullYear())
+console.log(mydate.toDateString())
+console.log(mydate.toLocaleDateString())
+console.log(mydate.getTimezoneOffset())
+console.log(mydate.toTimeString())
+
+
+
