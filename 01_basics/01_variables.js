c19fae30c03af54905a97ea65127f746b9ec0da1
adityadebnath009 <debnathaditya2005@gmail.com>
2024-07-15 05:23:18 +0000

    stagged changes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,9 +3,12 @@ let accountEmail = "example@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
 let accountState
-
+accountId = 24325353;
+// The const keyword does not make the variable immutable, but it prevents re-assignment. If the const variable holds an object, the properties of that object can still be changed.
+// Use const when you need to declare a variable that you don’t want to be reassigned after its initial value is set.
 
 console.log(accountId)
+//Use this when you want to print multuple things
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
 
 accountCity="Bengaluru"
