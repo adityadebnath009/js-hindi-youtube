852b9a68bce54d3ddab72fad5a780ef1ed3c5ce2
adityadebnath009 <debnathaditya2005@gmail.com>
2024-07-15 16:24:37 +0000

    container config done

---
+++ 02_basics/05_functions.js
@@ -0,0 +1,7 @@
+// A function expression can be stored in a variable:
+const x = function (a, b) {return a * b};
+
+// After a function expression has been stored in a variable, the variable can be used as a function:
+
+let z = x(4, 3);
+console.log()
