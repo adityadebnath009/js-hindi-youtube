2d2772e2c5924939b958d2d22779c7c5f90f659d
Aditya Debnath <debnathaditya2005@gmail.com>
2024-07-31 17:07:40 +0530

    Update 02_datatypes.js

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -15,7 +15,9 @@ let age = 19
 // null----standalone value
 // undefined
 // Symbol
+//JavaScript does NOT work this way. Instead JavaScript is a dynamically-typed language.
 
+//In JavaScript, we don't have to (and in fact, even can't) declare the types of variables upfront. During the course of a program, we can easily change the types of values stored in a variable, and the interpreter won't complaint even for a second.
 
 console.log(null)
 console.log(undefined)
