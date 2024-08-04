ddf642048c034f32b0bfdd7bf7cca0f77c394a2e
Aditya Debnath <debnathaditya2005@gmail.com>
2024-08-04 17:27:38 +0530

    Update 02_arrays.js

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -6,3 +6,9 @@ let arr2 = [5,6,7,8,9]
 let arr3 =[...arr1,...arr2]
 console.log(arr3)
 
+
+// Note that as with sort(), reverse() mutates the original array and returns it back.
+//To convert an string into an array we use str.split(argument)
+
+//length ,indexOf ,slice ,includes ,join are common methods in both String and array
+
